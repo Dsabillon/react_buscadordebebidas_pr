d678e027d7286a44b8d1def6068a60bcb34cfac9
DavidS <dsabillon.94@gmail.com>
2020-04-26 01:15:35 -0600

    Version1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//ContextProvider
+import CategoriasProvider from "./Context/CategoriasContext";
+import RecetasProvider from "./Context/RecetasContext";
+import ModalProvider from "./Context/ModalContext";
+
+//Components
+import Header from "./Components/Header";
+import Formulario from "./Components/Formulario";
+import ListaRecetas from "./Components/ListaRecetas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CategoriasProvider>
+      <RecetasProvider>
+        <ModalProvider>
+          <Header />
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
+            <ListaRecetas />
+          </div>
+        </ModalProvider>
+      </RecetasProvider>
+    </CategoriasProvider>
   );
 }
 
